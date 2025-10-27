8ac524462ad5f790cd566407190efa6f4e7e9ad6
Hannah <hannah.kimura@intersystems.com>
2025-10-27 16:49:22 -0400

    add core and expr to c and rust bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -21,7 +21,37 @@
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
-//!
+//! ```
+//! let code = r#"
+//!  if 1 set x = 3 set set = 3
+//!  if 1 { set x = 3 } else { w 2 }
+//!  set x = 3
+//! "#;
+//! 
+//! let mut parser = tree_sitter::Parser::new();
+//! let language = tree_sitter_objectscript::LANGUAGE_OBJECTSCRIPT_CORE;
+//! parser
+//!     .set_language(&language.into())
+//!     .expect("Error loading Objectscript Core parser");
+//! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
+//! 
+//! ```
+//! 
+//! ```
+//! let code = r#"
+//!  $CASE($P($ZE,">",1)_">", "<INTERRUPT>":"KeyboardInterrupt", "<SYNTAX>":"TypeError", :"RuntimeError")
+//! "#;
+//! 
+//! let mut parser = tree_sitter::Parser::new();
+//! let language = tree_sitter_objectscript::LANGUAGE_OBJECTSCRIPT_EXPR;
+//! parser
+//!     .set_language(&language.into())
+//!     .expect("Error loading Objectscript Expr parser");
+//! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
+//! 
+//! ```
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 //! [language func]: fn.language.html
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
@@ -31,43 +61,73 @@ use tree_sitter_language::LanguageFn;
 
 extern "C" {
     fn tree_sitter_objectscript() -> *const ();
+    fn tree_sitter_objectscript_core() -> *const ();
+    fn tree_sitter_objectscript_expr() -> *const ();
+    
 }
 
 /// The tree-sitter [`LanguageFn`] for ObjectScript.
 ///
 /// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
 pub const LANGUAGE_OBJECTSCRIPT: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_objectscript) };
+pub const LANGUAGE_OBJECTSCRIPT_CORE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_objectscript_core) };
+pub const LANGUAGE_OBJECTSCRIPT_EXPR: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_objectscript_expr) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const OBJECTSCRIPT_NODE_TYPES: &str = include_str!("../../udl/src/node-types.json");
+pub const OBJECTSCRIPT_CORE_NODE_TYPES: &str = include_str!("../../core/src/node-types.json");
+pub const OBJECTSCRIPT_EXPR_NODE_TYPES: &str = include_str!("../../expr/src/node-types.json");
 
 /// The syntax highlighting query for ObjectScript.
-pub const HIGHLIGHTS_QUERY: &str = concat!(
+pub const OBJECTSCRIPT_HIGHLIGHTS_QUERY: &str = concat!(
 include_str!("../../expr/queries/highlights.scm"), "\n",
 include_str!("../../core/queries/highlights.scm"), "\n",
 include_str!("../../udl/queries/highlights.scm"),
 );
 
+/// The syntax highlighting query for ObjectScript Core.
+pub const OBJECTSCRIPT_CORE_HIGHLIGHTS_QUERY: &str = concat!(
+include_str!("../../expr/queries/highlights.scm"), "\n",
+include_str!("../../core/queries/highlights.scm")
+);
+
+/// The syntax highlighting query for ObjectScript Expr.
+pub const OBJECTSCRIPT_EXPR_HIGHLIGHTS_QUERY: &str = "../../expr/queries/highlights.scm";
+
 /// The symbol tagging query for ObjectScript.
-pub const TAGS_QUERY: &str = concat!(
+pub const OBJECTSCRIPT_TAGS_QUERY: &str = concat!(
 include_str!("../../expr/queries/tags.scm"), "\n",
 include_str!("../../core/queries/tags.scm"), "\n",
 include_str!("../../udl/queries/tags.scm"),
 );
 
-/// INJECTIONS
-pub const INJECTIONS_QUERY: &str = concat!(
+/// The symbol tagging query for ObjectScript Core.
+pub const OBJECTSCRIPT_CORE_TAGS_QUERY: &str = concat!(
+include_str!("../../expr/queries/tags.scm"), "\n",
+include_str!("../../core/queries/tags.scm"));
+
+/// The symbol tagging query for ObjectScript Expr.
+pub const OBJECTSCRIPT_EXPR_TAGS_QUERY: &str = "../../expr/queries/tags.scm";
+
+/// Injections for ObjectScript.
+pub const OBJECTSCRIPT_INJECTIONS_QUERY: &str = concat!(
 include_str!("../../core/queries/injections.scm"), "\n",
 include_str!("../../udl/queries/injections.scm"),
 );
 
-/// indentation
+/// Injections for ObjectScript Core.
+pub const OBJECTSCRIPT_CORE_INJECTIONS_QUERY: &str = "../../core/queries/injections.scm";
+
+/// Indentation for ObjectScript.
 pub const INDENTS_QUERY: &str = concat!(
 include_str!("../../core/queries/indents.scm"), "\n",
 include_str!("../../udl/queries/indents.scm"),
 );
+
+/// Indentation for ObjectScript Core.
+pub const OBJECTSCRIPT_CORE_INDENTS_QUERY: &str = "../../core/queries/indents.scm";
 
 /// The local var syntax highlighting query for ObjectScript.
 pub const LOCALS_QUERY: &str = concat!(
@@ -76,13 +136,38 @@ include_str!("../../core/queries/locals.scm"), "\n",
 include_str!("../../udl/queries/locals.scm"),
 );
 
+/// The local var syntax highlighting query for ObjectScript Core.
+pub const OBJECTSCRIPT_CORE_LOCALS_QUERY: &str = concat!(
+include_str!("../../expr/queries/locals.scm"), "\n",
+include_str!("../../core/queries/locals.scm")
+);
+
+/// The local var syntax highlighting query for ObjectScript Expr.
+pub const OBJECTSCRIPT_EXPR_LOCALS_QUERY: &str = "../../expr/queries/locals.scm";
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_can_load_grammar() {
+    fn test_can_load_objectscript_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(&super::LANGUAGE_OBJECTSCRIPT.into())
             .expect("Error loading Objectscript parser");
+    }
+
+    #[test]
+    fn test_can_load_objectscript_core_grammar() {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&super::LANGUAGE_OBJECTSCRIPT_CORE.into())
+            .expect("Error loading Objectscript Core parser");
+    }
+
+    #[test]
+    fn test_can_load_objectscript_expr_grammar() {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&super::LANGUAGE_OBJECTSCRIPT_EXPR.into())
+            .expect("Error loading Objectscript Expr parser");
     }
 }
