d1c767446f84a8836d0a60549e0694706151ffdc
Hannah <hannah.kimura@intersystems.com>
2025-11-11 10:06:27 -0500

    bump version to publish changes

--- core/src/parser.c
+++ core/src/parser.c
@@ -467966,7 +467966,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_objectscript_core(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 1,
-      .patch_version = 2,
+      .patch_version = 3,
     },
   };
   return &language;
--- udl/src/parser.c
+++ udl/src/parser.c
@@ -528325,7 +528325,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_objectscript(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 1,
-      .patch_version = 2,
+      .patch_version = 3,
     },
   };
   return &language;
