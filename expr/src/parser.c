7aba644dcc71b81563ef31cdd9188d33cd836b73
Hannah <hannah.kimura@intersystems.com>
2026-03-03 13:59:20 -0500

    docs: update documentation and bump cargo version to v1.4.0

--- expr/src/parser.c
+++ expr/src/parser.c
@@ -23597,8 +23597,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_objectscript_expr(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 1,
-      .minor_version = 3,
-      .patch_version = 7,
+      .minor_version = 4,
+      .patch_version = 0,
     },
   };
   return &language;
